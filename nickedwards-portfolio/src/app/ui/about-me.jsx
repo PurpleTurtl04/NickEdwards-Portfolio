c8eb3e45e04cb6ffeaa1d3d6c804df4057aa132e
Nick Edwards <nledwa01@gmail.com>
2026-03-01 08:14:53 -0600

    Modified svg names. Continued updates on About Me section on the home page. Added content and structure for small, medium, and large screens.

--- nickedwards-portfolio/src/app/ui/about-me.jsx
+++ nickedwards-portfolio/src/app/ui/about-me.jsx
@@ -1,14 +1,16 @@
+import Image from 'next/image';
+
+import Accent from '../../../public/Accent.png';
+import Eight from '../../../public/Eight.svg';
+import Dev from '../../../public/Dev.svg';
+import Design from '../../../public/Design.svg';
+
 function AboutMe() {
     return (
-        <div className='bg-base-200 container mx-auto grid grid-cols-2'>
-            <div className='flex'>
-                <div>8+ Years of Experience</div>
-                <div>Web/Software Development</div>
-                <div>Web Design</div>
-            </div>
-            <div className='flex flex-col'>
-                <h2 className='text-2xl md:text-3xl lg:text-4xl'>About Me</h2>
-                <p>
+        <div className='container mx-auto my-15 grid grid-cols-1 items-center justify-center gap-6 lg:grid-cols-2'>
+            <div className='flex flex-col items-center lg:order-2'>
+                <h2 className='text-center text-3xl lg:text-4xl'>About Me</h2>
+                <p className='max-w-lg text-justify lg:text-left'>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                     Duis non nisi eget ipsum lacinia varius et maximus magna.
                     Sed hendrerit purus imperdiet elementum condimentum. Mauris
@@ -18,6 +20,49 @@ function AboutMe() {
                     eros egestas, sed dignissim est lacinia. Fusce rutrum ligula
                     sed eros vestibulum, efficitur pulvinar risus posuere.{' '}
                 </p>
+            </div>
+            <div className='flex justify-center gap-4'>
+                <div>
+                    <Image
+                        src={Accent}
+                        width={10}
+                        height={355}
+                        alt='Three lines spaced with two circles inbetween them'
+                        className='hidden scale-75 lg:block'
+                    />
+                </div>
+                <div className='flex flex-col justify-center gap-6 md:flex-row lg:order-1 lg:flex-col'>
+                    <div className='flex flex-col-reverse items-center justify-center gap-2 lg:flex-row lg:justify-start lg:gap-4'>
+                        <Image
+                            src={Eight}
+                            width={75}
+                            height={75}
+                            alt='The number eight inside a square'
+                            className='max-w-15'
+                        />
+                        <p>8+ Years of Experience</p>
+                    </div>
+                    <div className='flex flex-col-reverse items-center justify-center gap-2 lg:flex-row lg:justify-start lg:gap-4'>
+                        <Image
+                            src={Dev}
+                            width={75}
+                            height={75}
+                            alt='Angle brackets in a square'
+                            className='max-w-15'
+                        />
+                        <p>Web/Software Development</p>
+                    </div>
+                    <div className='flex flex-col-reverse items-center justify-center gap-2 lg:flex-row lg:justify-start lg:gap-4'>
+                        <Image
+                            src={Design}
+                            width={75}
+                            height={75}
+                            alt='A pencil and ruler making an X'
+                            className='max-w-15'
+                        />
+                        <p>Web Design</p>
+                    </div>
+                </div>
             </div>
         </div>
     );
